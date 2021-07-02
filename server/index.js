4fcfa68a57216cc7a44029bfac5cfb66202ac257
suwidnyana <grayfullbuster522@gmail.com>
2021-07-02 20:40:16 +0800

    make it better

--- server/index.js
+++ server/index.js
@@ -1,37 +1,28 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
-const app = express()
+const app = express();
 
-
-
-import apps from './modules/cors.js'
-import connectMongoose from './modules/mongoose.js'
-import router from './routes/index.js'
+import apps from "./modules/cors.js";
+import connectMongoose from "./modules/mongoose.js";
+import router from "./routes/index.js";
 
 // import postRoutes from './routes/posts.js';
 // import userRoutes from './routes/user.js'
-
-
 
 // app.use(express.json({limit: "30mb", extended: true}));
 // app.use(express.urlencoded({limit: "30mb", extended: true}));
 
 // app.use(cors());
 
-
-
-
 // app.use('/posts', postRoutes);
 // app.use('/user', userRoutes );
 
-const corss = apps(app)
-const mongooseexecute = connectMongoose()
-const routers = router(app)
+app.get("/", (req, res) => {
+  res.send(" Hello to memories API");
+});
 
-
-
-
-
-
+const corss = apps(app);
+const mongooseexecute = connectMongoose();
+const routers = router(app);
