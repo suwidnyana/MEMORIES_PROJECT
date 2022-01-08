5de8e83838eb3d0a099904b34f542b2920c39806
suwidnyana <grayfullbuster522@gmail.com>
2022-01-09 02:07:59 +0800

    init app.js

--- server/app.js
+++ server/app.js
@@ -2,7 +2,21 @@ import express from 'express'
 import bodyParser from 'body-parser	'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
+dotenv.config()
 
 const app =  express()
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+
+const CONNECTION_URL = process.env.ATLAS_URI;
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
