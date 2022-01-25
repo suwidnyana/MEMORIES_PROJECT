5c6e03a3032e44856b6f55139509b112502f56a8
suwidnyana <grayfullbuster522@gmail.com>
2022-01-26 00:21:37 +0800

    add jsx structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,29 @@
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import Posts from "../src/components/Posts/Posts"
+import Form from "../src/components/Form/Form"
+
+import memories from "./images/memories.png"
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Memories</Typography>
+        <img src={memories} alt='' height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify='space-between' alignItems='center' spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,11 @@
+import Post from "./Post/Post"
+const Posts = () => {
+    return (
+        <>
+            <h1>Posts</h1>
+            <Post />
+        </>
+    )
+};
+
+export default Posts
